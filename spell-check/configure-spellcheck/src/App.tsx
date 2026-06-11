2a19412b6eab3d2999bd57351180b400e0724804
SaranSF4541 <saran.sf4541@gmail.com>
2026-06-11 13:42:09 +0530

    updating Spell checker intialization code

--- spell-check/configure-spellcheck/src/App.tsx
+++ spell-check/configure-spellcheck/src/App.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react';
-
-import { DocumentEditorContainerComponent, Toolbar } from '@syncfusion/ej2-react-documenteditor';
-
+import { DocumentEditorContainerComponent, SpellChecker, Toolbar } from '@syncfusion/ej2-react-documenteditor';
 DocumentEditorContainerComponent.Inject(Toolbar);
-
 function App() {
     const containerRef = React.useRef(null);
-
     React.useEffect(() => {
         if (containerRef.current) {
-            // Accessing spell checker
-            let spellChecker = (containerRef.current as DocumentEditorContainerComponent).documentEditor.spellChecker;
-            // Set language id to map dictionary in server side
+            let container: DocumentEditorContainerComponent = containerRef.current as DocumentEditorContainerComponent;
+            // Get the SpellChecker instance from DocumentEditorContainer.
+            let spellChecker: SpellChecker = container.documentEditor.spellChecker;
+            // set the language ID for spell checker. Here, 1033 is the language ID for English (United States).
             spellChecker.languageID = 1033;
+            // remove the underline for misspelled words.
             spellChecker.removeUnderline = false;
-            // Allow suggestion for misspelled word
+            // Allow suggestion for misspelled word 
             spellChecker.allowSpellCheckAndSuggestion = true;
         }
     }, []);
-
     return (
         <DocumentEditorContainerComponent
-            id="container"
             height="590px"
             ref={containerRef}
+            // Use the following service URL only for demo purposes
             serviceUrl="https://document.syncfusion.com/web-services/docx-editor/api/documenteditor/"
             enableToolbar={true}
             enableSpellCheck={true}
